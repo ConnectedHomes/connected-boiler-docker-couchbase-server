b82c34ac042150d71378739571eb8c640a6ba875
Andrew Martin <sublimino@gmail.com>
2014-07-03 15:27:13 +0100

    Update tests for linux environment

--- test/acceptance/test.sh
+++ test/acceptance/test.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+
+
 gulp docker:run
 
 LAST_IMAGE=$(docker ps -l -q)
 
-IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' ${LAST_IMAGE})
+if [[ ${DOCKER_HOST:-} != '' ]]; then
+ IP=$(echo ${DOCKER_HOST} | sed -e 's#.*//##g' -e 's#:.*##g')
+else
+ IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' ${LAST_IMAGE})
+fi
+
+sleep 12
 
 docker logs ${LAST_IMAGE}
 
-COMMAND="curl --silent --connect-timeout 10 http://${IP}:8091/pools/default/buckets"
+COMMAND="curl --silent --connect-timeout 3 http://${IP}:8091/pools/default/buckets"
 echo "Running: ${COMMAND}"
-${COMMAND}
-STATUS_CODE=$?
 
-if [[ ${STATUS_CODE} -gt 0 ]]; then
+EXIT_CODE=0
+if ! ${COMMAND}; then
     echo "curl failed. Sleeping before retry"
     sleep 10
 
     docker logs ${LAST_IMAGE}
     echo "Retrying ${COMMAND}"
 
-    ${COMMAND}
-    STATUS_CODE=$?
-fi
-
-if [[ ${STATUS_CODE} -gt 0 ]]; then
-    echo "Failed to curl couchbase"
+    if ! ${COMMAND}; then
+      echo "Failed to curl couchbase"
+    fi
+    EXIT_CODE=1
 fi
 
 # stop last container
 docker stop $(docker ps -l -q)
 
-echo "docker ps | awk '{print $1}' | \xargs docker stop"
+echo "docker ps | awk '{print \$1}' | \xargs docker stop"
 
-exit ${STATUS_CODE}
+exit ${EXIT_CODE}
